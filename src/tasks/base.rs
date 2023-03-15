70dc5ad0e03d51cbb0bdc309665f98c7104bbd08
Vasco Costa <vascomacosta@gmail.com>
2023-03-15 22:19:29 +0000

    Use Tokio async functions to open/read out.txt

--- src/tasks/base.rs
+++ src/tasks/base.rs
@@ -3,8 +3,9 @@ use irc::client::Client;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use std::{fs::OpenOptions, io::BufRead, io::BufReader};
+use tokio::io::AsyncBufReadExt;
 use tokio::sync::Mutex;
+use tokio::{fs::OpenOptions, io::BufReader};
 
 pub async fn external_message(client: Arc<Mutex<Client>>) {
     loop {
@@ -13,6 +14,7 @@ pub async fn external_message(client: Arc<Mutex<Client>>) {
             .write(true)
             .truncate(true)
             .open("out.txt")
+            .await
         {
             Ok(file) => file,
             Err(error) => {
@@ -24,19 +26,18 @@ pub async fn external_message(client: Arc<Mutex<Client>>) {
             }
         };
 
-        let mut reader = BufReader::new(&file);
+        let mut reader = BufReader::new(file);
 
         loop {
             let mut line = String::new();
 
-            match reader.read_line(&mut line) {
+            match reader.read_line(&mut line).await {
                 Ok(0) => {
                     thread::sleep(Duration::from_secs(1));
+
                     continue;
                 }
                 Ok(_) => {
-                    file.set_len(0).unwrap();
-
                     if line.len() > 2 && line.starts_with("#") {
                         let split_line: Vec<&str> = line.split_ascii_whitespace().collect();
 
