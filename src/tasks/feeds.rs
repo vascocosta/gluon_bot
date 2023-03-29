27173f570812c0362efa600d4116873adf042564
Vasco Costa <vascomacosta@gmail.com>
2023-03-29 20:20:10 +0100

    Avoid cloning url

--- src/tasks/feeds.rs
+++ src/tasks/feeds.rs
@@ -79,7 +79,7 @@ pub async fn feeds(
                 let url = feed.url;
                 let channel = feed.channel;
                 let mut last_modified = feed.published;
-                let feed = match reqwest::get(url.clone()).await {
+                let feed = match reqwest::get(&url).await {
                     Ok(response) => match response.text().await {
                         Ok(feed) => feed,
                         Err(_) => return,
