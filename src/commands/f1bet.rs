65070a159afc674f4e573a709152c827cc321e38
Vasco Costa <vascomacosta@gmail.com>
2023-05-31 22:37:54 +0100

    Add score_bets to improve points readability

--- src/commands/f1bet.rs
+++ src/commands/f1bet.rs
@@ -4,6 +4,13 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+struct ScoringSystem {
+    boost: i32,
+    correct: i32,
+    fl: i32,
+    podium: i32,
+}
+
 struct Bet {
     race: String,
     nick: String,
@@ -37,37 +44,11 @@ impl CsvRecord for Bet {
     }
 }
 
-pub async fn points(options: &HashMap<String, String>, db: Arc<Mutex<Database>>) -> String {
-    let bets: Vec<Bet> = match db.lock().await.select("bets", |_| true) {
-        Ok(bets_result) => match bets_result {
-            Some(bets) => bets,
-            None => return String::from("Could not find any bets."),
-        },
-        Err(_) => return String::from("Could not find any bets."),
-    };
-    let results: Vec<Bet> = match db.lock().await.select("results", |_| true) {
-        Ok(bets_result) => match bets_result {
-            Some(bets) => bets,
-            None => return String::from("Could not find any results."),
-        },
-        Err(_) => return String::from("Could not find any results."),
-    };
-    let boost: i32 = match options.get("f1bet_boost") {
-        Some(boost) => boost.parse().unwrap_or(10),
-        None => 10,
-    };
-    let correct: i32 = match options.get("f1bet_correct") {
-        Some(correct) => correct.parse().unwrap_or(5),
-        None => 5,
-    };
-    let fl: i32 = match options.get("f1bet_fl") {
-        Some(fl) => fl.parse().unwrap_or(1),
-        None => 1,
-    };
-    let podium: i32 = match options.get("f1bet_podium") {
-        Some(podium) => podium.parse().unwrap_or(3),
-        None => 3,
-    };
+fn score_bets(
+    bets: Vec<Bet>,
+    results: Vec<Bet>,
+    scoring_system: ScoringSystem,
+) -> Vec<(String, i32)> {
     let bets_grouped: Vec<(_, Vec<Bet>)> = bets
         .into_iter()
         .sorted_by_key(|b: &Bet| b.nick.to_lowercase())
@@ -109,16 +90,22 @@ pub async fn points(options: &HashMap<String, String>, db: Arc<Mutex<Database>>)
                             .collect();
                         let podium_score: i32 = zipped
                             .iter()
-                            .map(|(b, r)| if b == r { correct } else { podium })
+                            .map(|(b, r)| {
+                                if b == r {
+                                    scoring_system.correct
+                                } else {
+                                    scoring_system.podium
+                                }
+                            })
                             .sum();
-                        let boost_score = if podium_score == (3 * correct) {
-                            podium_score + boost
+                        let boost_score = if podium_score == (3 * scoring_system.correct) {
+                            podium_score + scoring_system.boost
                         } else {
                             podium_score
                         };
 
                         if b.fl.to_lowercase() == results[0].fl.to_lowercase() {
-                            boost_score + fl
+                            boost_score + scoring_system.fl
                         } else {
                             boost_score
                         }
@@ -128,6 +115,52 @@ pub async fn points(options: &HashMap<String, String>, db: Arc<Mutex<Database>>)
         })
         .sorted_by(|a, b| b.1.cmp(&a.1))
         .collect();
+
+    bets_scored
+}
+
+pub async fn points(options: &HashMap<String, String>, db: Arc<Mutex<Database>>) -> String {
+    let bets: Vec<Bet> = match db.lock().await.select("bets", |_| true) {
+        Ok(bets_result) => match bets_result {
+            Some(bets) => bets,
+            None => return String::from("Could not find any bets."),
+        },
+        Err(_) => return String::from("Could not find any bets."),
+    };
+    let results: Vec<Bet> = match db.lock().await.select("results", |_| true) {
+        Ok(bets_result) => match bets_result {
+            Some(bets) => bets,
+            None => return String::from("Could not find any results."),
+        },
+        Err(_) => return String::from("Could not find any results."),
+    };
+    let boost: i32 = match options.get("f1bet_boost") {
+        Some(boost) => boost.parse().unwrap_or(10),
+        None => 10,
+    };
+    let correct: i32 = match options.get("f1bet_correct") {
+        Some(correct) => correct.parse().unwrap_or(5),
+        None => 5,
+    };
+    let fl: i32 = match options.get("f1bet_fl") {
+        Some(fl) => fl.parse().unwrap_or(1),
+        None => 1,
+    };
+    let podium: i32 = match options.get("f1bet_podium") {
+        Some(podium) => podium.parse().unwrap_or(3),
+        None => 3,
+    };
+
+    let bets_scored = score_bets(
+        bets,
+        results,
+        ScoringSystem {
+            boost,
+            correct,
+            fl,
+            podium,
+        },
+    );
 
     bets_scored
         .iter()
